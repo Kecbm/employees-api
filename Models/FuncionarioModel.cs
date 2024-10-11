0b510c7eed758ddf245deb990424f1d0fd620a2f
Klecianny Melo <kleciannymelo@gmail.com>
2024-10-11 17:41:15 -0300

    :sparkles: definindo o id como chave primária

--- Models/FuncionarioModel.cs
+++ Models/FuncionarioModel.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using EmployeesApi.Enums;
 
 namespace EmployeesApi.Models
 {
     public class FuncionarioModel
     {
+        [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
